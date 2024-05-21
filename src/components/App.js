331f8fd9436a601f250ba36756d12278d2324189
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-05-21 20:12:49 +0000

    running ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState();
+  function grab(e) {
+    setText(e.target.value);
+  }
   return (
-    <div>
-      <label>Enter your name:</label>
-      <input></input>
+    <div className="App">
+      <span>Enter your name:</span>
+      <br />
+      <br />
+      <input type="text" onChange={grab} />
+      <h4>{text}</h4>
     </div>
-  )
+  );
 }
 
 export default App
