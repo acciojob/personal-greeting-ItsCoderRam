d3b051469d3e6cad287089ae2fb8c345d58f8e25
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-05-22 10:22:24 +0000

    yehh

--- src/components/PersonalGreeting.js
+++ src/components/PersonalGreeting.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
+
 function PersonalGreeting() {
  const [name, setName] = useState('');
+
+const greetMessage = (e) => {
+    setName(e.target.value)
+}
  return (
    <div>
      <p>Enter your name:</p>
      <input
        type="text"
        value={name}
-       onChange={(e) => setName(e.target.value)}
+       onChange={greetMessage}
      />
      {name ? <p>Hello {name}!</p> : null}
    </div>
