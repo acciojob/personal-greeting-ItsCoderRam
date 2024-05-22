69443f8e79e4fb34590ceacd4583e632cb6f0b9a
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-05-22 09:47:27 +0000

    fnl

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,16 @@ import './../styles/App.css';
 
 const App = () => {
   const [text, setText] = useState("");
-  function grab(e) {
-    setText(e.target.value);
-  }
+  // function grab(e) {
+  //   setText(e.target.value);
+  // }
   return (
     <div className="App">
       <p>Enter your name:</p>
       <br />
       <br />
-      <input type="text" value={text} onChange={grab} />
-      {text ? <p>`Hello ${text}!`</p> : null}
+      <input type="text" value={text} onChange={(e) => setName(e.target.value)} />
+      {text ? <p>Hello {text}!</p> : null}
       <br/>
       <hr/>
     </div>
